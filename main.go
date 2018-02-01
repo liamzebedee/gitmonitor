90e207f1ef6eca3ed7c181f6dfc3068b80d196da
Liam Edwards-Playne <liamz@Help-Im-stuck-inside-a-speaker-I-dont-have-much-ti.local>
2018-02-01 10:30:54 +0000

    this is the GCHQ version

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os/user"
 	"time"
+	"path/filepath"
+	"os"
 	"github.com/mattn/go-zglob"
 	"github.com/fsnotify/fsnotify"
 )
@@ -14,8 +17,20 @@ func findGitRepos(homedir string) []string {
     if err != nil {
         log.Fatal(err)
 	}
+	
 	log.Println("Done")
 	return repos
+}
+
+func getDirsTree(dirPath string) (dirs []string) {
+	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			dirs = append(dirs, path)
+		}
+		return nil
+	})
+
+	return dirs
 }
 
 func main() {
@@ -32,18 +47,35 @@ func main() {
 	ticker := time.NewTicker(5 * time.Minute)
 	quit := make(chan struct{})
 	go func() {
-		// First run.
-		for _, repo := range findGitRepos(homedir) {
-			repoChan <- repo
+		sendPathsToWatcher := func() {
+			count := 0
+
+			for _, repo := range findGitRepos(homedir) {
+				dir := filepath.Dir(repo)
+
+				// dirs, _ := zglob.Glob(dir + "/**/")
+				dirs := getDirsTree(dir)
+
+				count += len(dirs)
+
+				repoChan <- dir
+				for _, subdir := range dirs {
+					fmt.Println(count, subdir)
+					repoChan <- subdir
+				}
+			}
+
+			fmt.Println(count)
 		}
+
+		// First run.
+		sendPathsToWatcher()
 
 		// Loop
 		for {
 		select {
 			case <- ticker.C:
-				for _, repo := range findGitRepos(homedir) {
-					repoChan <- repo
-				}
+				sendPathsToWatcher()
 			case <- quit:
 				ticker.Stop()
 				return
