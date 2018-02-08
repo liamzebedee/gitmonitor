e1f7217d0cce30478db9236c003e3c8bda680f4d
Liam Edwards-Playne <liamz@Help-Im-stuck-inside-a-speaker-I-dont-have-much-ti.local>
2018-02-08 19:56:56 +0000

    git ls-files polling

--- main.go
+++ main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
+	// "os"
+	"bytes"
+	"fmt"
 	"log"
 	"os/user"
 	"time"
+	"os/exec"
 	"github.com/mattn/go-zglob"
-	"github.com/fsnotify/fsnotify"
+	"path/filepath"
 )
+
 
 func findGitRepos(homedir string) []string {
 	repos, err := zglob.Glob(homedir + "/**/.git")
+	for i, repo := range repos {
+		repos[i] = filepath.Dir(repo)
+	}
 
     if err != nil {
         log.Fatal(err)
 	}
+
 	log.Println("Done")
 	return repos
+}
+
+func checkGitRepoUpdated(dir string) (output string, err error) {
+	cmd := exec.Command("git", "ls-files", "--modified", "--debug")
+	cmd.Dir = dir
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return stderr.String(), err
+	}
+	output = string(out)
+	return output, nil
+}
+
+type repoUpdateCheck struct {
+	repo string
+	output string
+	err error
 }
 
 func main() {
@@ -26,62 +54,87 @@ func main() {
 	
 	homedir := usr.HomeDir
 
-	reposToTrack := make(map[string]bool)
-	repoChan := make(chan string)
+	// refresh repos every 5 minutes
+	// start routine which checks them every 10s
+	// send done msg when we finish refreshing the next time
 
-	ticker := time.NewTicker(5 * time.Minute)
-	quit := make(chan struct{})
+	findGitReposTick := time.NewTicker(15 * time.Minute)
+	checkGitRepoUpdatedTick := time.NewTicker(1 * time.Minute)
+
+	gitReposToTrack := make(chan []string)
+
 	go func() {
-		// First run.
-		for _, repo := range findGitRepos(homedir) {
-			repoChan <- repo
-		}
+		var repos []string
+		repoLastCheckup := make(map[string]string)
+		repoUpdateCheckChan := make(chan repoUpdateCheck) 
+		ignore := make(map[string]bool)
 
-		// Loop
-		for {
-		select {
-			case <- ticker.C:
-				for _, repo := range findGitRepos(homedir) {
-					repoChan <- repo
-				}
-			case <- quit:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
-	
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	done := make(chan bool)
-	go func() {
 		for {
 			select {
-			case event := <-watcher.Events:
-				log.Println("event:", event)
-			case err := <-watcher.Errors:
-				log.Println("error:", err)
+			case <-checkGitRepoUpdatedTick.C:
+				for _, repo := range repos {
+					lastOutput, _ := repoLastCheckup[repo]
+
+					if ignore[repo] == true {
+						continue
+					}
+
+					go func(repo, lastOutput string) {
+						output, err := checkGitRepoUpdated(repo)
+						check := repoUpdateCheck{
+							repo, 
+							output,
+							err,
+						}
+						repoUpdateCheckChan <- check
+					}(repo, lastOutput)
+				}
+
+				break
+			
+			case check := <-repoUpdateCheckChan:
+				if check.err != nil {
+					fmt.Println("ERROR", check.err, check.output)
+					ignore[check.repo] = true
+					continue
+					// delete(check.repo, repos)
+				}
+
+				lastOutput, ok := repoLastCheckup[check.repo]
+				if !ok {
+					// First time checking.      
+					repoLastCheckup[check.repo] = check.output
+					continue
+				}
+
+				changed := (lastOutput != check.output)
+				if changed {
+					fmt.Println("UPDATED", check.repo)
+				}  
+				repoLastCheckup[check.repo] = check.output
+				break;
+
+			case repos = <-gitReposToTrack:
+				break
 			}
+
 		}
 	}()
+
+	refresh := func() {
+		fmt.Println("EVENT Refreshing")
+		repos := findGitRepos(homedir)
+		fmt.Println("EVENT Refreshed")
+		gitReposToTrack <- repos
+	}
+
+	// First run.
+	refresh()
 
 	for {
 		select {
-		case repo := <-repoChan:
-			if _, ok := reposToTrack[repo]; !ok {
-				err = watcher.Add(repo)
-				if err != nil {
-					log.Fatal(err)
-				} else {
-					reposToTrack[repo] = true
-				}
-			}
-		case <-done:
-			return
+		case <-findGitReposTick.C:
+			refresh()
 		}
 	}
 }
